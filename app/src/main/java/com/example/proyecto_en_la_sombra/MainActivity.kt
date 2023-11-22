3123578723853d4c9d855b9c89da837fa4f32260
Juan Manuel Mayo Beltrán <75370645+jmayobel@users.noreply.github.com>
2023-11-22 21:04:01 +0100

    Added Organization API

--- app/src/main/java/com/example/proyecto_en_la_sombra/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_en_la_sombra/MainActivity.kt
@@ -9,21 +9,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.proyecto_en_la_sombra.api.RetrofitService
+import com.example.proyecto_en_la_sombra.api.organizationsModel.Organization
 import com.example.proyecto_en_la_sombra.navigation.AppNavigation
 import kotlinx.coroutines.launch
 
-private const val auth = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqdDc4eWZaRmVQS3lHTTh0bUxwSFpkdVBlNHdpb2J1c3hBNGd2R3J4djVwOXhsTVJFeSIsImp0aSI6IjQ4M2ZmNzc3MWE0Nzk0MDFhYmY5MzdmMmMwOTZlZTZiODhkNWU0NDlkZWM2MGVjMjA1NjFmM2ZiZTU2YWY1OGNiNjM1NjZkMzhiZWUzZDI5IiwiaWF0IjoxNzAwNjUwNTcwLCJuYmYiOjE3MDA2NTA1NzAsImV4cCI6MTcwMDY1NDE3MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.I8IFjtKXGzNTGwEz6xSIeaUJX94674HTHRfUTJEsIl-B3igt1nFzhcKQECm5tYZjP8fHtTb4lVHS-OXp7MK7Qv9la_k86HcxJC7KLCELM5_uEAPqsZbTvqUF5whgwgixXCMM5NApk9bfhmREQTbyJ1xUpsN_aXVARRP1kk_jDlyk_q5gkB5_kUOzhrEQIsScqGQpHcjrFy9DHFQ2JG2umXzyoZ0Sr8p8Nm8CF4Z3onlwCqJKYRJCj8TDwI2puSenSR0DBaSZVryOOgPK-lRQi8HHkvHmjNHx3i30Ad8b1E1UxFclg3vv47LKwsd3PzWofOuUAABWf9Om2z6HpICiNQ"
+private const val grant_type = "client_credentials"
+private const val client_id = "jt78yfZFePKyGM8tmLpHZduPe4wiobusxA4gvGrxv5p9xlMREy"
+private const val client_secret = "ADto7Ake8ThsPUe57IprbsLHORd29qhZynDdw1ej"
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        val service = RetrofitService.RetrofitServiceFactory.makeRetrofitService()
         lifecycleScope.launch {
-            val animal = service.getAnimals("69771579")
+
+            val authResponse = service.login(grant_type,client_id,client_secret)
+            val auth = "Bearer ${authResponse.access_token}"
+            println(auth)
+            val animal = service.getAnimals(auth,"69771579")
             println(animal)
 
-            val listanimals = service.getRandom("random")
+            val listanimals = service.getAnimalsRandom(auth,"random")
             println(listanimals)
+
+            val listOrganizations = service.getOrganizations(auth)
+            println(listOrganizations)
+
+            val Organization = service.getUniqueOrganization(auth,"WI535")
+            println(Organization)
         }
             setContent {
                 AppNavigation()
---
+++ app/src/main/java/com/example/proyecto_en_la_sombra/api/organizationsModel/Adoption.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_en_la_sombra.api.organizationsModel
+
+data class Adoption(
+    val policy: Any,
+    val url: Any
+)
