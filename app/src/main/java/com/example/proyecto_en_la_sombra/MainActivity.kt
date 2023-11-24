a9a5253afbfe396b5425bb5e318e85d3c00b3543
anavaspe <adriannavas091@gmail.com>
2023-11-24 10:29:34 +0100

    resuelto bug de insercion en la base de datos del mismo objeto en el Main Activity

--- app/src/main/java/com/example/proyecto_en_la_sombra/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_en_la_sombra/MainActivity.kt
@@ -34,8 +34,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
        val service = RetrofitService.RetrofitServiceFactory.makeRetrofitService()
        val room : AplicacionDB = AplicacionDB.getInstance(context = this)
+
         lifecycleScope.launch {
-            room.clienteDAO().insertCliente(Cliente(1,"Manuel",null))
+            //room.clienteDAO().insertCliente(Cliente(1,"Manuel",null))
              val clientes: List<Cliente> = room.clienteDAO().getClientes()
 
             Log.i("Numero de clientes almacenados en la base de datos: ",clientes.size.toString())
