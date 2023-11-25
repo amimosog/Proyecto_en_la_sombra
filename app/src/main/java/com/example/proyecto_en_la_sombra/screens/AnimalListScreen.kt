2bebf95ac7fd080c8a0982b32ab7d23be3290a14
alexm <amimosog@alumnos.unex.es>
2023-11-25 11:03:16 +0100

    Arreglo de el error cuando un animal no tiene imagenes

--- app/src/main/java/com/example/proyecto_en_la_sombra/screens/AnimalListScreen.kt
+++ app/src/main/java/com/example/proyecto_en_la_sombra/screens/AnimalListScreen.kt
@@ -79,7 +79,12 @@ fun listElement(animal: Animal, navController: NavController){
         }
     ) {
         Spacer(modifier = Modifier.height(5.dp))
-        imageElements(animal.photos[0].full)
+        imageElements(
+            if(animal.photos.isNotEmpty())
+                animal.photos[0].full
+            else
+                "https://play-lh.googleusercontent.com/QuYkQAkLt5OpBAIabNdIGmd8HKwK58tfqmKNvw2UF69pb4jkojQG9za9l3nLfhv2N5U"
+        )
         iconElements()
         textElements(animal)
     }
