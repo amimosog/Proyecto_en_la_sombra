dce14a0f6f4e9c6a4ae42ed0f67483b8f896282c
Ángel Peña <619angel16@gmail.com>
2023-11-26 14:33:54 +0100

    OrgListScreen.kt modificada, todo centrado

--- app/src/main/java/com/example/proyecto_en_la_sombra/screens/OrgListScreen.kt
+++ app/src/main/java/com/example/proyecto_en_la_sombra/screens/OrgListScreen.kt
@@ -3,9 +3,11 @@ package com.example.proyecto_en_la_sombra.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -41,7 +44,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
 @Composable
-fun OrganizationList(navController: NavController){
+fun OrganizationList(navController: NavController) {
     var result by remember { mutableStateOf<OrganizationsRemoteModel?>(null) }
     LaunchedEffect(true) {
         val service = RetrofitService.RetrofitServiceFactory.makeRetrofitService()
@@ -49,11 +52,13 @@ fun OrganizationList(navController: NavController){
         result = query.await()
     }
 
-    if (result != null){
-        Row (
+    if (result != null) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
-        ){
+                .fillMaxWidth()
+        ) {
             Text(
                 text = "Lista de Organizaciones",
                 color = MaterialTheme.colorScheme.onBackground,
@@ -61,26 +66,28 @@ fun OrganizationList(navController: NavController){
                 textAlign = TextAlign.Center,
             )
         }
-        LazyColumn (
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(top = 35.dp)
                 .fillMaxWidth()
-        ){
+        ) {
             result?.organizations?.let {
-                items(it){ org ->
+                items(it) { org ->
                     mostrarOrg(org, navController)
                     Spacer(modifier = Modifier.height(10.dp))
                 }
             }
         }
-    }
 
+    }
 }
 
 @Composable
-fun mostrarOrg(org: Organization, navController: NavController){
+fun mostrarOrg(org: Organization, navController: NavController) {
     var expanded by remember { mutableStateOf(false) }
-    Row(modifier = Modifier
+    Row(
+        modifier = Modifier
             .fillMaxWidth(0.90f)
             .clip(RoundedCornerShape(15.dp))
             .background(Color.Gray)
@@ -97,12 +104,12 @@ fun mostrarOrg(org: Organization, navController: NavController){
 }
 
 @Composable
-fun orgLogo(org: Organization){
+fun orgLogo(org: Organization) {
     AsyncImage(
-        model = if(org.photos.isNotEmpty())
-                    org.photos[0].full
-                else
-                    "https://play-lh.googleusercontent.com/QuYkQAkLt5OpBAIabNdIGmd8HKwK58tfqmKNvw2UF69pb4jkojQG9za9l3nLfhv2N5U",
+        model = if (org.photos.isNotEmpty())
+            org.photos[0].full
+        else
+            "https://play-lh.googleusercontent.com/QuYkQAkLt5OpBAIabNdIGmd8HKwK58tfqmKNvw2UF69pb4jkojQG9za9l3nLfhv2N5U",
         placeholder = painterResource(R.drawable.ic_launcher_foreground),
         contentDescription = "Organization logo",
         modifier = Modifier
@@ -113,26 +120,29 @@ fun orgLogo(org: Organization){
 }
 
 @Composable
-fun orgTexts(org: Organization){
+fun orgTexts(org: Organization) {
     Column {
-        Text(text = if(org.name != null)
-                        org.name
-                    else
-                        "No tiene un nombre asignado",
+        Text(
+            text = if (org.name != null)
+                org.name
+            else
+                "No tiene un nombre asignado",
             color = Color.White,
             style = MaterialTheme.typography.bodyMedium
         )
-        Text(text = if(org.phone != null)
-                        org.phone
-                    else
-                        "No tiene un telefono asignado",
+        Text(
+            text = if (org.phone != null)
+                org.phone
+            else
+                "No tiene un telefono asignado",
             color = Color.Blue,
             style = MaterialTheme.typography.bodySmall
         )
-        Text(text = if(org.email != null)
-                        org.email
-                    else
-                        "No tiene un email asignado",
+        Text(
+            text = if (org.email != null)
+                org.email
+            else
+                "No tiene un email asignado",
             color = Color.Yellow,
             style = MaterialTheme.typography.bodySmall
         )
